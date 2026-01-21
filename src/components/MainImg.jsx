768c0b4c70c780c36d9068b0e81bb642f1687b10
lee ji yoon <leejiyoon@lees-iMac.local>
2026-01-21 15:04:41 +0900

    이미지 크기 조정 및 반응형 배경 적용

--- src/components/MainImg.jsx
+++ src/components/MainImg.jsx
@@ -20,17 +20,19 @@ export default function MainImg() {
       <div className="absolute bg-[rgba(0,0,0,0.1)] inset-0 mix-blend-overlay" data-name="Background image" data-node-id="206:15693" />
       <div className="absolute bg-gradient-to-t from-[6.064%] from-[rgba(68,66,255,0)] inset-0 mix-blend-overlay to-[#4442ff]" data-name="Background image" data-node-id="206:15694" />
       <div className="absolute bg-gradient-to-t from-[6.064%] from-[rgba(68,66,255,0)] inset-0 opacity-60 to-black" data-name="Background image" data-node-id="206:15695" />
-      <div className="absolute flex flex-col gap-[13px] items-center left-[50%] top-[13.2%] -translate-x-1/2 w-[65.6%] max-w-[236px]" data-name="main_text" data-node-id="206:15696">
+      <div className="absolute flex flex-col gap-[2px] items-center left-[50%] top-[13.2%] -translate-x-1/2 w-[65.6%] max-w-[236px]" data-name="main_text" data-node-id="206:15696">
         <div className="relative shrink-0 w-full max-w-[236px]" data-node-id="206:15697">
-          <img alt="상품 받고" className="w-full h-auto object-contain" src={imgText1} />
+          <img alt="상품 받고" className="w-full h-auto object-contain" style={{ transform: 'scale(0.5)', transformOrigin: 'center' }} src={imgText1} />
         </div>
         <div className="h-[70px] sm:h-[70.074px] relative shrink-0 w-[92.7%] max-w-[218.909px]" data-name="img_text" data-node-id="206:15698">
           <div className="absolute inset-0" data-name="Main image" data-node-id="206:15699">
             <img alt="" className="absolute inset-0 max-w-none object-contain pointer-events-none size-full" src={imgMainImage} />
           </div>
         </div>
-        <div className="relative shrink-0 w-full max-w-[236px]" data-node-id="206:15700">
-          <img alt="받아가세요!" className="w-full h-auto object-contain" src={imgText2} />
+        <div className="h-[70px] sm:h-[70.074px] relative shrink-0 w-[92.7%] max-w-[218.909px]" data-node-id="206:15700">
+          <div className="absolute inset-0">
+            <img alt="받아가세요!" className="absolute inset-0 max-w-none object-contain pointer-events-none size-full" src={imgText2} />
+          </div>
         </div>
       </div>
     </div>
